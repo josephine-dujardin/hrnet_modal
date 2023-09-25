158a811d48bc30565dc5fd9af4070240fb2fdb5b
DUJARDIN Joséphine <josephine83230@gmail.com>
2023-09-25 19:54:28 +0200

    new npm version 1.0.7

--- modal.jsx
+++ modal.jsx
@@ -1,12 +1,10 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 
 /**
  * A modal component for creating an employee profile.
  * @returns {JSX.Element} - The rendered modal component.
  */
-function Modal() {
-
-    const [isOpen, setIsOpen] = useState(false);
+function Modal({ isOpen, openClose, content }) {
 
     /**
      * Opens the modal.
@@ -14,42 +12,17 @@ function Modal() {
      * @function
      * @returns {void}
      */
-    const openModal = () => {
-        setIsOpen(true);
-    };
-
-    useEffect(() => {
-        /**
-         * Adds or removes the 'modal-open' class to the body element based on the modal state.
-         *
-         * @function
-         * @returns {void}
-         */
-        if (isOpen) {
-            document.body.classList.add('modal-open');
-        } else {
-            document.body.classList.remove('modal-open');
-        }
-    }, [isOpen]);
-
-    /**
-     * Closes the modal.
-     *
-     * @function
-     * @returns {void}
-     */
-    const closeModal = () => {
-        setIsOpen(false);
+    const toggleModal = () => {
+        openClose();
     };
 
     return (
         React.createElement('div', { className: 'modal-container' },
-            React.createElement('button', { className: 'save', onClick: openModal }, 'Save'),
             isOpen && (
                 React.createElement('div', { className: 'modal' },
                     React.createElement('div', { className: 'modal-content' },
-                        React.createElement('span', { className: 'close', onClick: closeModal }, '\u00D7'), // \u00D7' represents the '×' character
-                        React.createElement('p', { className: 'modal-text' }, 'Employee Created!')
+                        React.createElement('span', { className: 'close', onClick: toggleModal }, '\u00D7'), // \u00D7' represents the '×' character
+                        React.createElement('p', { className: 'modal-text' }, content)
                     )
                 )
             )
